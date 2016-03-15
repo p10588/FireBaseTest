5812f0820331da917e95b8dcf902ed4a0d8e0576
p10588 <p10588chain@gmail.com>
2016-03-15 10:56:21 +0800

    Create the project
    create the project

---
+++ FireBaseTest/FireBaseTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireBaseTest
+//
+//  Created by CHAIN on 2016/3/15.
+//  Copyright © 2016年 CHAIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
